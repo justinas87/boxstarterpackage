16dbccde155201a92a9e0ae5ba891a5f1c8257ed
Justinas <just.sunklodas@gmail.com>
2017-07-27 15:04:52 +0300

    sa
    Bandymai

---
+++ naujikompai.ps1
@@ -0,0 +1,19 @@
+
+$Boxstarter.RebootOk=$true # Allow reboots?
+$Boxstarter.NoPassword=$false # Is this a machine with no login password?
+$Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
+
+Set-ExplorerOptions - showHiddenFilesFolderDrives -showProtectedOSFIles -showFileExtensions -EnableShowFullPathInTitleBar
+Enable-RemoteDesktop
+
+cinst -y firefox
+cinst -y googlechrome
+cinst -y jre8
+cinst -y winrar
+cinst -y 7zip.install 
+cinst -y silverlight
+cinst -y ammyy-admin
+cinst -y Paint.Net
+cinst -y flashplayerplugin
+cinst -y adobereader
+
