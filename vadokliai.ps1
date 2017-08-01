b565f5741a5e57b6109fc2661e570b60f300ffcf
Justinas <just.sunklodas@gmail.com>
2017-08-01 10:49:25 +0300

    Create vadokliai.ps1

---
+++ vadokliai.ps1
@@ -0,0 +1,26 @@
+$Boxstarter.RebootOk=$false # Allow reboots?
+$Boxstarter.NoPassword=$false # Is this a machine with no login password?
+$Boxstarter.AutoLogin=$false # Save my password securely and auto-login after a rebo
+Set-ExplorerOptions - showHiddenFilesFolderDrives -showProtectedOSFIles -showFileExtensions -EnableShowFullPathInTitleBar
+Enable-RemoteDesktop
+Update-ExecutionPolicy Unrestricted
+
+choco install javaruntime
+choco install java.jdk
+choco install ccleaner
+choco install flashplayerplugin
+choco install Paint.Net
+choco install vlc
+choco install firefox
+choco install googlechrome
+choco install powershell4
+choco install adobereader
+choco install winrar
+choco install 7zip.install 
+choco install silverlight
+choco install notepadplusplus
+choco install libreoffice
+choco install produkey.install 
+choco install ammyy-admin
+
+add-computer -domainname prs.local; #reikalingas restart | reikes pabandyti " ; restart-computer"
