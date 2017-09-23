56456648edf30b7478071138eaac8c2a9666257e
Justinas <just.sunklodas@gmail.com>
2017-09-24 02:06:39 +0300

    Create kuklus.ps1

---
+++ kuklus.ps1
@@ -0,0 +1,22 @@
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
+Enable-RemoteDesktop
+
+cinst windirstat
+cinst sysinternals
+cinst paint.ne
+cinst putty
+cinst coretemp
+cinst jdk7
+cinst filezilla
+choco install sumatrapdf
+choco install googlechrome
+choco install 7zip
+choco install beyondcompare
+choco install baretail
+choco install vlc
+choco install gitkraken
+choco install curl
+choco install atom
+choco install visualstudiocode 
+
+Set-ExecutionPolicy -Force Unrestricted
