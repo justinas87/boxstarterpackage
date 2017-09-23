e79b75d817d2d8e7e253c9dafc60a10b9db2fdfe
Justinas <just.sunklodas@gmail.com>
2017-09-24 02:52:58 +0300

    atom editoriaus pluginai

--- kuklus.ps1
+++ kuklus.ps1
@@ -24,3 +24,5 @@ choco install gitkraken
 choco install curl
 choco install atom
 choco install visualstudiocode
+# atom pluginai
+apm install activate-power-mode advanced-open-file atom-material-ui autocomplete-modules autocomplete-paths color-picker editor-stats esformatter file-icons fold-functions gitignore-snippets highlight-selected imdone-atom language-gitignore language-javascript-jsx linter linter-eslint linter-flow mac-fix-path merge-conflicts minimap minimap-autohide minimap-bookmarks minimap-find-and-replace minimap-git-diff minimap-hide minimap-highlight-selected minimap-linter minimap-pigments minimap-selection npm-install pane-layout-plus pigments react-snippets regex-railroad-diagram regex-tester sort-lines terminal-plus tomorrow-night-eighties-syntax
