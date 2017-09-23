9d43eafba99daa5ed350a12fe550aca2dcb3edcb
Justinas <just.sunklodas@gmail.com>
2017-09-24 02:21:27 +0300

    Update kuklus.ps1

--- kuklus.ps1
+++ kuklus.ps1
@@ -1,5 +1,9 @@
-Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
+$Boxstarter.RebootOk=$false # Allow reboots?
+$Boxstarter.NoPassword=$false # Is this a machine with no login password?
+$Boxstarter.AutoLogin=$false # Save my password securely and auto-login after a rebo
+Set-ExplorerOptions - showHiddenFilesFolderDrives -showProtectedOSFIles -showFileExtensions -EnableShowFullPathInTitleBar
 Enable-RemoteDesktop
+Update-ExecutionPolicy Unrestricted
 
 cinst windirstat
 cinst sysinternals
@@ -13,11 +17,10 @@ choco install sumatrapdf
 choco install googlechrome
 choco install 7zip
 choco install beyondcompare
+choco install wget
 choco install baretail
 choco install vlc
 choco install gitkraken
 choco install curl
 choco install atom
-choco install visualstudiocode 
-
-Set-ExecutionPolicy -Force Unrestricted
+choco install visualstudiocode
